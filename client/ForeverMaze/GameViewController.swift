88107a63d60099d094340b188be72c29daa30dda
Zane Claes <zane.claes@airbnb.com>
2015-11-20 11:19:13 -0800

    Initial asset cleanup

--- client/ForeverMaze/GameViewController.swift
+++ client/ForeverMaze/GameViewController.swift
@@ -9,45 +9,27 @@
 import UIKit
 import SpriteKit
 
+import UIKit
+import SpriteKit
+
 class GameViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    let scene = GameScene(size: view.bounds.size)
+    let skView = view as! SKView
+    skView.showsFPS = true
+    skView.showsNodeCount = true
+    skView.ignoresSiblingOrder = true
+    scene.scaleMode = .ResizeFill
+    skView.presentScene(scene)
+  }
 
-        if let scene = GameScene(fileNamed:"GameScene") {
-            // Configure the view.
-            let skView = self.view as! SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
-            
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
-            skView.ignoresSiblingOrder = true
-            
-            /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .AspectFill
-            
-            skView.presentScene(scene)
-        }
-    }
+  override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
+    return .Landscape
+  }
 
-    override func shouldAutorotate() -> Bool {
-        return true
-    }
-
-    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
-        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return .AllButUpsideDown
-        } else {
-            return .All
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
-    override func prefersStatusBarHidden() -> Bool {
-        return true
-    }
+  override func prefersStatusBarHidden() -> Bool {
+    return true
+  }
 }
